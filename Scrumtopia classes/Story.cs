f1cff0d4e251d72d3a111f530e3df7ee38ffe03b
Theodor O. Perrier <theodor.per@gmail.com>
2020-05-06 09:53:57 +0200

    Generalt opsætning

--- Scrumtopia classes/Story.cs
+++ Scrumtopia classes/Story.cs
@@ -17,6 +17,12 @@ namespace Scrumtopia_classes
         public string Story_State { get; set; }
         public User Story_Referee { get; set; }
         public User Story_Asignee { get; set; }
+        public List<Task> Tasks { get; set; }
 
+
+        public override string ToString()
+        {
+            return $"{nameof(Project_Id)}: {Project_Id}, {nameof(Sprint_Id)}: {Sprint_Id}, {nameof(Category)}: {Category}, {nameof(Story_Name)}: {Story_Name}, {nameof(Story_description)}: {Story_description}, {nameof(Story_Points)}: {Story_Points}, {nameof(Story_Priority)}: {Story_Priority}, {nameof(Story_State)}: {Story_State}, {nameof(Story_Referee)}: {Story_Referee}, {nameof(Story_Asignee)}: {Story_Asignee}, {nameof(Tasks)}: {Tasks}";
+        }
     }
 }
---
+++ Scrumtopia/Converter/DateTimeConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scrumtopia.Converter
+{
+    class DateTimeConverter
+    { 
+        
+        /// <summary>
+        /// Converts a day and time to a datetíme
+        /// </summary>
+        /// <param name="date"> Must be a type of date time offset</param>
+        /// <param name="time">must be of type timespan</param>
+        /// <returns></returns>
+        public static DateTime ConverterToDateTime(DateTimeOffset date, TimeSpan time) => new DateTime(date.Year, date.Month, date.Day, time.Hours, time.Minutes, 0);// Bruges til oprettelse af nyt event, så databasen kan bruge værdierne
+
+
+        public static DateTimeOffset ConvertToDate(DateTime dateTime) => new DateTimeOffset(dateTime);
+        public static TimeSpan ConvertToTime(DateTime dateTime) => new TimeSpan(dateTime.Hour, dateTime.Minute, dateTime.Second);
+    }
+}
